9b0083c0d2a8541dfd9c3a89d4a38c5a67e72420
WensenLi234 <wli9204@bths.edu>
2022-09-27 21:44:19 -0400

    Finished calculations and display, added number formatting and panels for the display.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        String panel = "============";
+        NumberFormat dollarFormat = new DecimalFormat("#.00");
         // Input
         Scanner input = new Scanner(System.in);
         System.out.print("Please input the cost of your bill: ");
@@ -10,9 +14,22 @@ public class Main {
         System.out.print("Please input the number of people splitting the bill: ");
         int numPeople = input.nextInt();
         // Output calculations
-        double costPerPerson = (int) (bill / numPeople * 100 + .9);
-        double tipPerPerson = costPerPerson * tip / 100;
+        double costPerPerson = (int) (bill / numPeople * 100) / 100.00;
+        double tipPerPerson = (int) (costPerPerson * tip) / 100.00;
+        double totalPerPerson = costPerPerson + tipPerPerson;
         // Display
         System.out.println(costPerPerson);
+        System.out.println(panel);
+        System.out.println("Total tip amount: $" + dollarFormat.format(tipPerPerson * numPeople));
+        System.out.println(panel);
+        System.out.println(panel);
+        System.out.println("Total bill: $" + dollarFormat.format(totalPerPerson * numPeople));
+        System.out.println(panel);
+        System.out.println(panel);
+        System.out.println("Tip per person: $" + dollarFormat.format(tipPerPerson));
+        System.out.println(panel);
+        System.out.println(panel);
+        System.out.println("Total cost per person: $" + dollarFormat.format(totalPerPerson));
+        System.out.println(panel);
     }
 }
